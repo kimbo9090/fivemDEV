5154180a9e232510ba8ee9523b66f9ad328e28b5
CapitanRetraso <capitanretraso@gmail.com>
2019-05-19 20:39:55 +0200

    Added Essentialmode and the ability to teleport to specific locations

---
+++ resources/teleport/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.Locations = {
+
+    mazebank =          {name="Maze Bank", x= -75.17, y = -819.24, z = 326.18},
+    pier =              {name="Del Perro Pier", x= -1634.823, y = -993.65, z = 13.01},
+    airport =           {name="Los Santos International Airport", x = -1018.337, y = -2886.25, z = 13.96}
+
+}
--- resources/teleport/teleport-c.lua
+++ resources/teleport/teleport-c.lua
@@ -1,15 +1,33 @@
 RegisterCommand('tp', function(source, args)
 
+--Command usage
   if (args[1] == nil and args[2] == nil and args[3] == nil) then --Check if all arguments are missing
     TriggerEvent('chat:addMessage', {
         args = { '^1 No coordinates specified!' }
     })
     TriggerEvent('chat:addMessage', {
-      args = { '^7 Usage: ^2 /tp ^* x y z' }
+      args = { '^7 Usage: ^2 /tp ^* x y z ^r ^7 or ^2 /tp ^* location' }
     })
     return
     end
   
+
+  --Teleport by location name
+  if (args[2] == nil and args[3] == nil) then
+    if (Config.Locations[args[1]] ~= nil) then --check for a valid location
+      TriggerEvent('chat:addMessage', {
+        args = {'^7 Teleporting to: ^3'.. Config.Locations[args[1]].name}})
+        SetPedCoordsKeepVehicle(PlayerPedId(), Config.Locations[args[1]].x, Config.Locations[args[1]].y, Config.Locations[args[1]].z)
+        return
+      else
+        TriggerEvent('chat:addMessage', {
+          args = {'^1 Location not found'}})
+      return
+    end
+  end
+  
+
+
   local x = tonumber(args[1])
   local y = tonumber(args[2])
   local z = tonumber(args[3])
@@ -23,7 +41,7 @@ RegisterCommand('tp', function(source, args)
 end)
 
 
-
+--Coords command
 RegisterCommand('coords', function(source, args)
 
   local playerCoords = GetEntityCoords(PlayerPedId())
